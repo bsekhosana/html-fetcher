a98e0d6ce65efe9f77a466c8a135434734a85b90
Bruno Sekhosana <bsekhosana@users.noreply.github.com>
2024-06-18 10:48:50 +0200

    Update server.js

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ app.get('/fetch-html', async (req, res) => {
     }
 
     try {
-        // const browser = await puppeteer.launch();
-        // const page = await browser.newPage();
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
         // await page.goto(url, { waitUntil: 'networkidle2' });
 
         // const browser = await puppeteer.launch({
@@ -24,23 +24,23 @@ app.get('/fetch-html', async (req, res) => {
         //   });
 
         
-        puppeteer.launch({
-          executablePath: '/opt/render/.cache/puppeteer/chrome-headless-shell/linux-126.0.6478.61' // '/opt/render/.cache/puppeteer/chrome/linux-126.0.6478.61'//'/opt/render/.cache/puppeteer/chrome/linux-126.0.6478.61/chrome-linux64/chrome'
-        }).then(async browser => {
-          // Your code here
-            const page = await browser.newPage();
-            await page.goto(url, { waitUntil: 'domcontentloaded' });
+        // puppeteer.launch({
+        //   executablePath: '/opt/render/.cache/puppeteer/chrome-headless-shell/linux-126.0.6478.61' // '/opt/render/.cache/puppeteer/chrome/linux-126.0.6478.61'//'/opt/render/.cache/puppeteer/chrome/linux-126.0.6478.61/chrome-linux64/chrome'
+        // }).then(async browser => {
+        //   // Your code here
+        //     const page = await browser.newPage();
+        //     await page.goto(url, { waitUntil: 'domcontentloaded' });
         
-            const content = await page.content();
-            await browser.close();
-            res.send(content);
-        });
+        //     const content = await page.content();
+        //     await browser.close();
+        //     res.send(content);
+        // });
         
-        // await page.goto(url, { waitUntil: 'domcontentloaded' });
+        await page.goto(url, { waitUntil: 'domcontentloaded' });
         
-        // const content = await page.content();
-        // await browser.close();
-        // res.send(content);
+        const content = await page.content();
+        await browser.close();
+        res.send(content);
     } catch (error) {
         res.status(500).send(`Error fetching HTML: ${error.message}`);
     }
