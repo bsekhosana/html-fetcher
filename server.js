a66cdb9d4a92fe2d298ae7620712037db7e9d72a
Bruno Sekhosana <bsekhosana@users.noreply.github.com>
2024-06-18 10:58:32 +0200

    Update server.js

--- server.js
+++ server.js
@@ -11,33 +11,14 @@ app.get('/fetch-html', async (req, res) => {
     }
 
     try {
-        const browser = await puppeteer.launch();
-        const page = await browser.newPage();
-        // await page.goto(url, { waitUntil: 'networkidle2' });
-
-        // const browser = await puppeteer.launch({
-        //     headless: "new",
-        //     args: [
-        //       '--no-sandbox',
-        //       '--disable-dev-shm-usage'
-        //     ],
-        //   });
-
+        // const browser = await puppeteer.launch();
+        const browser = await puppeteer.launch({
+              args: ['--no-sandbox', '--disable-setuid-sandbox'],
+              executablePath: '/usr/bin/google-chrome-stable' // or the path where Chrome is installed
+            });
         
-        // puppeteer.launch({
-        //   executablePath: '/opt/render/.cache/puppeteer/chrome-headless-shell/linux-126.0.6478.61' // '/opt/render/.cache/puppeteer/chrome/linux-126.0.6478.61'//'/opt/render/.cache/puppeteer/chrome/linux-126.0.6478.61/chrome-linux64/chrome'
-        // }).then(async browser => {
-        //   // Your code here
-        //     const page = await browser.newPage();
-        //     await page.goto(url, { waitUntil: 'domcontentloaded' });
-        
-        //     const content = await page.content();
-        //     await browser.close();
-        //     res.send(content);
-        // });
-        
-        await page.goto(url, { waitUntil: 'domcontentloaded' });
-        
+        const page = await browser.newPage();        
+        await page.goto(url, { waitUntil: 'domcontentloaded' });  
         const content = await page.content();
         await browser.close();
         res.send(content);
